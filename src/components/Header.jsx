8501383a21e226a03b8f7ed104b60749e33fa6fc
Paula Fenner <fenner.paula@gmail.com>
2024-12-05 17:59:24 +0100

    Implemented functionality that the user adds an ingredient to the form, and once the button is clicked the state is updated.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <header>
-        <img src="../public/chef-claude-icon.svg" className="logo" alt="chef claude logo" />
+        <img src="/chef-claude-icon.svg" className="logo" alt="chef claude logo" />
         <h1>Chef Claude</h1>
       </header>
     </>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+
 export default function Main() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const ingredientList = ingredients.map((ingredient) => <li key={ingredient}>{ingredient}</li>);
+
+  function handleSubmit(e) {
+    /* ```ingredients.push(newIngredient);``` how we would add with vanila JS */
+    setIngredients((prevIngredient) => [
+      ...prevIngredient,
+      newIngredient,
+    ]); /* this is how we add items to an array in React */
+
+    e.preventDefault(); /* Prevent form submission, trigger onClick instead*/
+    const formData = new FormData(e.currentTarget);
+    const newIngredient = formData.get('ingredient');
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
-        <input type="text" placeholder="e.g. milk" aria-label="Add an ingredient" />
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="e.g. milk" aria-label="Add an ingredient" name="ingredient" />
         <button className="add-btn">Add ingredient</button>
       </form>
+      <ul>{ingredientList}</ul>
     </main>
   );
 }
